0b05f4269bf5dc807a5bc05cca3ddc27fc7d52dc
Keith Brown <s.keith.brown@gmail.com>
2017-07-21 10:58:01 -0400

    job #9568 - remove unused imports

--- src/org.xtuml.bp.welcome.test/src/org/xtuml/bp/welcome/test/WelcomeTestSuite.java
+++ src/org.xtuml.bp.welcome.test/src/org/xtuml/bp/welcome/test/WelcomeTestSuite.java
@@ -1,25 +1,14 @@
 package org.xtuml.bp.welcome.test;
 
-import org.eclipse.core.runtime.CoreException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.xtuml.bp.core.Ooaofooa;
-import org.xtuml.bp.core.util.WorkspaceUtil;
 
 //=====================================================================
+//Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+//use this file except in compliance with the License.  You may obtain a copy 
+//of the License at
 //
-//File:      $RCSfile: WelcomeTestSuite.java,v $
-//Version:   $Revision: 1.11 $
-//Modified:  $Date: 2013/05/10 13:25:52 $
-//
-//(c) Copyright 2004-2014 by Mentor Graphics Corp. All rights reserved.
-//
-//=====================================================================
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not 
-// use this file except in compliance with the License.  You may obtain a copy 
-// of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software 
 // distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
@@ -27,9 +16,6 @@ import org.xtuml.bp.core.util.WorkspaceUtil;
 // License for the specific language governing permissions and limitations under
 // the License.
 //=====================================================================
-
-
-import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
