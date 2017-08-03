ff101608e69cda94578456b3f0a626677be2fc95
Levi Starrett <levi@roxsoftware.com>
2017-08-03 15:31:15 -0400

    job #9561 remove GPS test from canvas suite 2

--- src/org.xtuml.bp.ui.canvas.test/src/GlobalsCanvasSuite2.java
+++ src/org.xtuml.bp.ui.canvas.test/src/GlobalsCanvasSuite2.java
@@ -31,7 +31,6 @@ import org.xtuml.bp.ui.canvas.test.anchors.GraphicalAnchorTests_0;
 import org.xtuml.bp.ui.canvas.test.movement.ConnectorMoveTests_0;
 import org.xtuml.bp.ui.canvas.test.routing.RectilinearRoutingTests_0;
 import org.xtuml.bp.ui.canvas.test.assoc.AssociationMove_0;
-import org.xtuml.bp.ui.canvas.test.assoc.AssociationMoveGPS;
 
 import junit.framework.TestSuite;
 
@@ -46,8 +45,7 @@ import junit.framework.TestSuite;
     ConnectorPolicyTests.class,
     ConnectorMoveTests_0.class,
     RectilinearRoutingTests_0.class,
-    AssociationMove_0.class,
-    AssociationMoveGPS.class,
+    AssociationMove_0.class
 })
 public class GlobalsCanvasSuite2 extends TestSuite {
 
