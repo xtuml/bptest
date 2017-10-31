90a64513ca674ec5c2a8dd8d2e21bb2ab2a7367f
Levi Starrett <levi@roxsoftware.com>
2017-10-31 19:59:36 -0400

    job #9870 remove auto complete tests from OALGlobals because they are already in the pom file

--- src/org.xtuml.bp.als.oal.test/src/OALGlobalsTestSuite_Generics.java
+++ src/org.xtuml.bp.als.oal.test/src/OALGlobalsTestSuite_Generics.java
@@ -72,7 +72,6 @@ import org.xtuml.bp.als.oal.test.TestSelect_Generics;
 import org.xtuml.bp.als.oal.test.TestSelect_Generics_Ordered;
 import org.xtuml.bp.als.oal.test.TestStructuredDataType_Generics;
 import org.xtuml.bp.als.oal.test.VisibilityParserTest;
-import org.xtuml.bp.als.oal.test.completion.OalAutoCompleteTestSuite;
 import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.common.BridgePointPreferencesStore;
 
@@ -131,8 +130,7 @@ import junit.framework.TestSuite;
 	TestAllowInterfaceNameInICMsg_Generics.class,
 	PolyEventTest.class,
 	SupportConstantsViaIPRTests.class,
-	TestScopedConstants.class,
-  OalAutoCompleteTestSuite.class
+	TestScopedConstants.class
 })
 public class OALGlobalsTestSuite_Generics extends TestSuite {
 	/**
