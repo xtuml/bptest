95ecfe871d30d298b1a7b307e27210c6d37855af
Keith Brown <s.keith.brown@gmail.com>
2018-08-06 10:28:59 -0400

    job #10341 - manually select a clear spot on the canvas to paste.  The previous code ended up pasting _inside_ what should have been the sibling package of our pasted element.

--- src/org.xtuml.bp.ui.canvas.test/src/org/xtuml/bp/ui/canvas/test/CanvasCopyPasteTests.java
+++ src/org.xtuml.bp.ui.canvas.test/src/org/xtuml/bp/ui/canvas/test/CanvasCopyPasteTests.java
@@ -88,7 +88,7 @@ public class CanvasCopyPasteTests extends CanvasTest {
 		ce = ((ModelEditor) PlatformUI.getWorkbench()
 				.getActiveWorkbenchWindow().getActivePage().getActiveEditor())
 				.getGraphicalEditor();
-		UITestingUtilities.pasteClipboardContents(UITestingUtilities.getClearPoint(ce), ce);
+		UITestingUtilities.pasteClipboardContents(new Point(400,100), ce);
 		validateOrGenerateResults(ce, generateResults);
 		Package_c newDtPackage = Package_c.getOneEP_PKGOnR1405(m_sys, new ClassQueryInterface_c() {
 		
