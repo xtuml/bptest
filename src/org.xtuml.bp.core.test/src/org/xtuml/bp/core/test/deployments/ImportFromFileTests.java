723f15d62a24df6b1dc7776d107f30c0627c6ba4
Levi Starrett <levi@roxsoftware.com>
2018-12-19 14:56:52 -0500

    job #10525 add import to deployment from file tests

---
+++ src/org.xtuml.bp.core.test/src/org/xtuml/bp/core/test/deployments/ImportFromFileTests.java
@@ -0,0 +1,78 @@
+package org.xtuml.bp.core.test.deployments;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.xtuml.bp.core.DataType_c;
+import org.xtuml.bp.core.Deployment_c;
+import org.xtuml.bp.core.Ooaofooa;
+import org.xtuml.bp.core.TerminatorServiceParameter_c;
+import org.xtuml.bp.core.TerminatorService_c;
+import org.xtuml.bp.core.Terminator_c;
+import org.xtuml.bp.test.common.BaseTest;
+import org.xtuml.bp.test.common.OrderedRunner;
+
+@RunWith(OrderedRunner.class)
+public class ImportFromFileTests extends BaseTest {
+
+    private static final String[] PROJECT_NAMES = { "DeploymentsTests", "DeploymentsDomains" };
+    private static final String MOD_FILE = "/DeploymentsDomains/masl/DeploymentsDomain1/DeploymentsDomain1.mod";
+    private static final String INT_FILE = "/DeploymentsDomains/masl/DeploymentsDomain1/DeploymentsDomain1.int";
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        for (String project : PROJECT_NAMES) {
+            loadProject(project);
+        }
+        modelRoot = Ooaofooa
+                .getInstance("/DeploymentsTests/models/DeploymentsTests/DeploymentsTests/DeploymentsTests.xtuml");
+        m_sys = modelRoot.getRoot();
+    }
+
+    @Test
+    public void testImportFromIntFile() throws Exception {
+        testImportFromFile(INT_FILE);
+    }
+
+    @Test
+    public void testImportFromModFile() throws Exception {
+        testImportFromFile(MOD_FILE);
+    }
+
+    private void testImportFromFile(String filename) throws Exception {
+
+        // get the deployment
+        Deployment_c deployment = Deployment_c.DeploymentInstance(modelRoot);
+        assertNotNull(deployment);
+
+        // trigger the import action
+        deployment.Importfromfile(getTestModelRespositoryLocation() + filename);
+
+        // check the resulting instances
+        Terminator_c providedTerm = Terminator_c.getOneD_TERMOnR1650(deployment,
+                (selected) -> "DeploymentsDomain1".equals(((Terminator_c) selected).getName()));
+        assertTrue("Provided terminator missing.", null != providedTerm && providedTerm.getProvider());
+        TerminatorService_c provSvc = TerminatorService_c.getOneD_TSVCOnR1651(providedTerm,
+                (selected) -> "public_service1".equals(((TerminatorService_c) selected).getName()));
+        assertNotNull("Provided terminator service missing.", provSvc);
+        TerminatorServiceParameter_c provSvcParam = TerminatorServiceParameter_c.getOneD_TSPARMOnR1652(provSvc,
+                (selected) -> "real_param".equals(((TerminatorServiceParameter_c) selected).getName()));
+        assertNotNull("Provided terminator service parameter missing.", provSvcParam);
+        DataType_c s_dt = DataType_c.getOneS_DTOnR1653(provSvcParam);
+        assertTrue("Incorrect parameter type.", null != s_dt && "real".equals(s_dt.getName()));
+        Terminator_c requiredTerm = Terminator_c.getOneD_TERMOnR1650(deployment,
+                (selected) -> "DeploymentsDomain1::term1".equals(((Terminator_c) selected).getName()));
+        assertTrue("Required terminator missing.", null != requiredTerm && !requiredTerm.getProvider());
+        TerminatorService_c reqSvc = TerminatorService_c.getOneD_TSVCOnR1651(requiredTerm,
+                (selected) -> "term_service1".equals(((TerminatorService_c) selected).getName()));
+        assertNotNull("Required terminator service missing.", reqSvc);
+        TerminatorServiceParameter_c reqSvcParam = TerminatorServiceParameter_c.getOneD_TSPARMOnR1652(reqSvc,
+                (selected) -> "enum_param".equals(((TerminatorServiceParameter_c) selected).getName()));
+        assertNotNull("Required terminator service parameter missing.", reqSvcParam);
+        s_dt = DataType_c.getOneS_DTOnR1653(reqSvcParam);
+        assertTrue("Incorrect parameter type.", null != s_dt && "DeploymentsDomain1::MyEnum".equals(s_dt.getName()));
+
+    }
+
+}
